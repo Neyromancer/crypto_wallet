e92507a45bd6d002e1bf53f0e54a28c4389b85d1
Neyromancer <dmitry.kormulev@yandex.ru>
2019-02-07 23:11:50 +0300

    Header file for data_loader_service created.

---
+++ src/client/data_loader_service/data_loader_service.h
@@ -0,0 +1,49 @@
+/// \file data_loader_service.h
+/// \brief Class responsible for loading data into DB.
+/// \author Dmitry Kormulev <dmitry.kormulev@yandex.ru>
+/// \version 1.0.0.0
+/// \data 07.02.2019
+
+#ifndef CRYPTO_WALLET_CLIENT_DATA_LOADER_SERVICE_H_
+#define CRYPTO_WALLET_CLIENT_DATA_LOADER_SERVICE_H_
+
+extern "C" {
+#include <sqlite3.h>
+}
+
+#include "socket_connection/unix_connection.h"
+
+/// \namespace crypto_wallet.
+/// \brief Project namespace.
+namespace crypto_wallet {
+/// \namespace client
+/// \brief Client namespace.
+namespace client {
+/// \class DataLoaderService data_loader_service.h.
+/// \brief Class responsible for loading data into DB.
+class DataLoaderService {
+ public:
+  /// \brief Daemonize process.
+  void Daemonize() const noexcept;
+
+  /// \brief Set and look after the connection.
+  void SetAndProcessConnection();
+
+  /// \brief Set data.
+  /// \param[in] data Data.
+  void SetData(std::string data);
+
+  /// \brief Return data.
+  /// \return Data.
+  inline std::string GetData() const noexcept {
+    return data_;
+  }
+  
+ private:
+  socket_communication::UnixConnection unix_connect_{};
+  std::string data_{};
+};
+}
+}
+
+#endif // CRYPTO_WALLET_CLIENT_DATA_LOADER_SERVICE_H_
