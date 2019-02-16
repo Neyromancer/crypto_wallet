87f06c11393285d2447890485347fd14c79e24be
Neyromancer <dmitry.kormulev@yandex.ru>
2019-02-16 22:20:57 +0300

    Methods for getting class instance modified.

--- src/client/data_loader_service/data_loader_service.cpp
+++ src/client/data_loader_service/data_loader_service.cpp
@@ -1,16 +1,38 @@
+/// \file data_loader_service.cpp
+/// \brief Source file containing class DataLoaderService methods definitions.
+/// \author Dmitry Kormulev <dmitry.kormulev@yandex.ru>
+/// \version 1.0.0.1
+/// \date 16.02.2019
+
+#include <iostream>
+
 #include "data_loader_service.h"
 
 namespace crypto_wallet {
 namespace client {
 
+DataLoaderService *DataLoaderService::data_loader_ = nullptr;
+
 DataLoaderService::DataLoaderService(std::string &&path) {
-  if (SQLITE_OK != sqlite3_open(path, &database))
+  if (SQLITE_OK != sqlite3_open(path.c_str(), &database_))
     // throw error becasuse connection was not set
-  if (!*database)
+    std::cout << "connection was not established" << std::endl;
+  //if (*database_ != NULL)
     // throw error because database was not created
+  //  std::cout << "database was not created" << std::endl;
+}
+
+DataLoaderService::DataLoaderService(const std::string &path) {
+  if (SQLITE_OK != sqlite3_open(path.c_str(), &database_))
+    // throw error becasuse connection was not set
+    std::cout << "connection was not established" << std::endl;
+  //if (*database_ != NULL)
+    // throw error because database was not created
+  //  std::cout << "database was not created" << std::endl;
 }
 
 void DataLoaderService::Daemonize() const noexcept {
+
 }
 
 void DataLoaderService::SetAndProcessConnection() {
@@ -23,7 +45,6 @@ void DataLoaderService::SetData(const std::string &data) {
 void DataLoaderService::SetData(std::string &&data) {
   data_ = std::move(data);
 }
-
 
 }
 }
--- src/client/data_loader_service/data_loader_service.h
+++ src/client/data_loader_service/data_loader_service.h
@@ -2,7 +2,7 @@
 /// \brief Class responsible for loading data into DB.
 /// \author Dmitry Kormulev <dmitry.kormulev@yandex.ru>
 /// \version 1.0.0.1
-/// \data 13.02.2019
+/// \date 16.02.2019
 
 #ifndef CRYPTO_WALLET_CLIENT_DATA_LOADER_SERVICE_H_
 #define CRYPTO_WALLET_CLIENT_DATA_LOADER_SERVICE_H_
@@ -28,34 +28,42 @@ class DataLoaderService {
  public:
   /// \brief Class DataLoaderService copy constructor.
   /// \param[in] data_loader_service Class DataLoaderService object.
-  DataLoaderService(const DataLoaderService &data_loader_service) = delete;
+  //DataLoaderService(const DataLoaderService &data_loader_service) = delete;
 
   /// \brief Class DataLoaderService copy assignment.
   /// \param[in] data_loader_service Class DataLoaderService object.
   /// \return DataLoaderService object.
-  DataLoaderService &operator=(const DataLoaderService &data_loader_service) =
-delete;
+  //DataLoaderService &operator=(const DataLoaderService &data_loader_service) =
+//delete;
   
-  /// \brief Connect to DB.
+  /// \brief Get instance of class DataLoaderService..
   /// \param[in] path Path to the DB.
-  /// \return Smart pointer to object of class DataLoaderService.
-  inline static std::shared_ptr<DataLoaderService> ConnectDataBase(std::string &&path) 
-  const noexcept {
-    return std::make_shared<DataLoaderService>(DataLoaderService(path));
+  /// \return Instance of class DataLoaderService.
+  inline static DataLoaderService *GetDataLoaderServiceInstance(std::string &&path) 
+  {
+    // data_loader_ = std::make_shared<DataLoaderService>(DataLoaderService(path));
+    if (!DataLoaderService::data_loader_)
+      DataLoaderService::data_loader_ = new DataLoaderService(path);
+    return data_loader_;
   }
 
-  /// \brief Connect to DB.
+  /// \brief Get instance of class DataLoaderService.
   /// \param[in] path Path to the DB.
-  /// \return Smart pointer to object of class DataLoaderService.
-  inline static std::shared_ptr<DataLoaderService> ConnectDataBase(const std::string &path) 
-  const noexcept {
-    return std::make_shared<DataLoaderService>(DataLoaderService(path));
+  /// \return Instance of class DataLoaderService.
+  inline static DataLoaderService *GetDataLoaderServiceInstance(const std::string &path) 
+  {
+    // data_loader_ = std::make_shared<DataLoaderService>(DataLoaderService(path));
+    if (!DataLoaderService::data_loader_)
+      DataLoaderService::data_loader_ = new DataLoaderService(path);
+    return data_loader_;
+    // static DataLoaderService data_loader(path);
+    // return &data_loader;
   }
 
   /// \brief Daemonize process.
   void Daemonize() const noexcept;
 
-  /// \brief Set and look after the connection.
+  /// \brief Set and look after the interprocess connection.
   void SetAndProcessConnection();
 
   /// \brief Set data.
@@ -72,18 +80,19 @@ delete;
   /// \brief Get DB.
   /// \return DB.
   inline sqlite3 &GetDataBase() const noexcept {
-    return database_;
+    return *database_;
   }
-  
- private:
-  /// \brief DataLoaderService constructor.
-  /// \param[in] path Path.
-  DataLoaderService(std::string &&path);
 
   /// \brief DataLoaderService destructor.
   ~DataLoaderService() {
     sqlite3_close(database_);
   }
+ 
+ private:
+  /// \brief DataLoaderService constructor.
+  /// \param[in] path Path.
+  DataLoaderService(std::string &&path);
+  DataLoaderService(const std::string &path);
 
   /// \brief Class DataLoaderService move constructor.
   /// \param[in] data_loader_service Class DataLoaderService object.
@@ -98,6 +107,8 @@ default;
   //socket_communication::UnixConnection unix_connect_{};
   std::string data_{};
   sqlite3 *database_{nullptr};
+  // static std::shared_ptr<DataLoaderService> data_loader_{};
+  static DataLoaderService *data_loader_;
 };
 }  // namespace client
 }  // namespace crypto_wallet
