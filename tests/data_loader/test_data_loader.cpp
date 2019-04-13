3435a8cdbe51d049783eec293eb762b786644ca5
Neyromancer <dmitry.kormulev@yandex.ru>
2019-04-13 20:45:05 +0300

    Created test_data_loader file for unit tests.

---
+++ tests/data_loader/test_data_loader.cpp
@@ -0,0 +1,19 @@
+/// \file test_data_loader.cpp
+/// \brief Unit tests for data_loader module.
+/// \author Dmitry Kormulev <dmitry.kormulev@yandex.ru>
+/// \version 1.0.0.1
+/// \date 13.04.2019
+
+#include <string>
+
+#include <gtest/gtest.h>
+
+#include "client/data_loader/data_loader.h"
+
+namespace crypto_wallet {
+namespace client {
+
+TEST (DataLoaderClassTest, 
+
+}
+}
