90c8c68f1a76ce1440adaa11ca116610c139df85
Neyromancer <dmitry.kormulev@yandex.ru>
2019-04-15 22:47:27 +0300

    Into file for testing data_loader main function added. Globale CMake file created.

--- tests/data_loader/test_data_loader.cpp
+++ tests/data_loader/test_data_loader.cpp
@@ -13,7 +13,47 @@
 namespace crypto_wallet {
 namespace client {
 
-TEST (DataLoaderClassTest, 
+TEST (DataLoaderClassTest, NameDataBase) {
+  std::string db_name{"test_db"};
+  DataLoader::GetDataLoaderInstance("test_db.db").SetDataBaseName(db_name);
+
+  EXPECT_EQ(db_name, DataLoader::GetDataLoaderInstance("test_db.db").GetDataBaseName());
+}
+
+TEST (DataLoaderClassTest, DBTableStructTemplate) {
+  std::string test_db_template_struct = "name1 LOGNTEXT, name2 LONGTEXT";
+  std::initializer_list<std::string> test_lst = {"name1 LOGNTEXT", "name2 LONGTEXT"};
+  DataLoader::GetDataLoaderInstance("test_db.db").SetDBTableStructTemplate(test_lst);
+  
+  EXPECT_EQ(test_db_template_struct,
+            DataLoader::GetDataLoaderInstance("test_db.db").GetDBTableStructTemplate());
+}
+
+TEST (DataLoaderClassTest, CreateDBTable) {
+  std::initializer_list<std::string> test_lst = {"name1 LOGNTEXT", "name2 LONGTEXT"};
+  DataLoader::GetDataLoaderInstance("test_db.db").SetDBTableStructTemplate(test_lst);
+
+  std::string db_table_name = "test_db_table";
+  DataLoader::GetDataLoaderInstance("test_db.db").CreateDBTable(db_table_name);
+  
+  EXPECT_EQ(db_table_name, DataLoader::GetDataLoaderInstance("test_db.db").GetDataBaseTableName());
+}
+
+TEST (DataLoaderClassTest, InsertIntoTable) {
+  std::initializer_list<std::string> test_lst = {"name1 LOGNTEXT", "name2 LONGTEXT"};
+  DataLoader::GetDataLoaderInstance("test_db.db").SetDBTableStructTemplate(test_lst);
+
+  std::string db_table_name = "test_db_table";
+  DataLoader::GetDataLoaderInstance("test_db.db").CreateDBTable(db_table_name);
+
+  std::initializer_list<std::string> test_val_lst = {"1", "2"};
+  DataLoader::GetDataLoaderInstance("test_db.db").InsertIntoTable(test_val_lst);
+}
 
 }
+}
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
